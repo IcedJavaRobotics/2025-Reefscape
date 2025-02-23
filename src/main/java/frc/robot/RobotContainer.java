25b926c203176ebb86162f8da6a75e6a90644407
IcedJavaRobotics <57970708+IcedJavaRobotics@users.noreply.github.com>
2025-02-23 13:29:24 -0500

    nickolus

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,16 +6,10 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
-import frc.robot.commands.CandleRed;
 import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.CandleSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.SwerveSubsystem;
-import swervelib.SwerveInputStream;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
@@ -27,43 +21,16 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private final CandleSubsystem candleSubsystem = new CandleSubsystem();
-  private final SwerveSubsystem drivebase = new SwerveSubsystem();
 
-   XboxController xboxController = new XboxController(0);
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
-
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
-    candleSubsystem.setCandleJavaBlue();
-    drivebase.setDefaultCommand(driveFieldOrientatedAngularVelocity);
   }
-
-
-
-SwerveInputStream driveAngularVelocity = SwerveInputStream.of(drivebase.getSwerveDrive(), 
-                                                              () -> m_driverController.getLeftY() * -1,
-                                                              () -> m_driverController.getLeftX() * -1)
-                                                              .withControllerRotationAxis(m_driverController::getRightX)
-                                                              .deadband(Constants.DEADBAND) 
-                                                              .scaleTranslation(0.8) //THIS VALUE IS CHANGABLE TO ADJUST ROBOT SPEED
-                                                              .allianceRelativeControl(true);
-
-  
-  SwerveInputStream driveDirectAngle = driveAngularVelocity.copy().withControllerHeadingAxis(m_driverController::getRightX,
-                                                                                            m_driverController::getRightY)
-                                                                                            .headingWhile(true);
-
-  Command driveFieldOrientatedDirectAngle = drivebase.driveFieldOrientated(driveDirectAngle);
-
-  Command driveFieldOrientatedAngularVelocity = drivebase.driveFieldOrientated(driveAngularVelocity);
-
-
 
   /**
    * Use this method to define your trigger->command mappings. Triggers can be created via the
@@ -78,9 +45,6 @@ SwerveInputStream driveAngularVelocity = SwerveInputStream.of(drivebase.getSwerv
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
     new Trigger(m_exampleSubsystem::exampleCondition)
         .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    new JoystickButton(xboxController, XboxController.Button.kB.value)
-        .whileTrue(new CandleRed(candleSubsystem));
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
