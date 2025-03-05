5c882bd4b6fdde160f8654beea6b75399e63fa6a
IcedJavaRobotics <IcedJavaRobotics@gmail.com>
2025-03-04 20:18:53 -0500

    jascpob

---
+++ src/main/java/frc/robot/subsystems/Networking.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Networking extends SubsystemBase {
+  /** Creates a new Networking. */
+  public Networking() {
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  private NetworkTable table;
+
+  public void robotInit() {
+    NetworkTableInstance inst = NetworkTableInstance.getDefault();
+    table = inst.getTable("AudioPlayer");
+    inst.putValue("Hallo World");
+
+  }
+}
