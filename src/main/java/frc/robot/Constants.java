2b82b9bcbaf7c70aa6ab06bccd55dc15ff86a305
Luke <mochatitanstudios@gmail.com>
2025-03-11 19:00:45 -0400

    changed values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -27,7 +27,7 @@ public final class Constants {
     // public static final String DEADBAND = "0.5";
     public static final double DEADBAND = 0.02;
     public static final double MAX_SPEED = Units.feetToMeters(4.5);
-    public static final int[] driverstationButtons = {14, 20, 30, 40}; //0 = top left, and then read it like a book
+    public static final int[] driverstationButtons = { 14, 20, 30, 40 }; // 0 = top left, and then read it like a book
   }
 
   public static class CandleConstants {
@@ -47,12 +47,12 @@ public final class Constants {
     public static final double SHOULDER_MOTOR_SPEED = 0.2;
 
     public static final double L1_SETPOINT = -78.788;
-    public static final double L2_SETPOINT = -32.677;
+    public static final double L2_SETPOINT = -37.433;
     public static final double L3_SETPOINT = 6.926;
     public static final double CORAL_STATION_SETPOINT = 2;
-    public static final double GROUND_SETPOINT = -70; //TODO
-    public static final double UPPER_ALGAE_SETPOINT = 10; //TODO
-    public static final double LOWER_ALGAE_SETPOINT = 10; //TODO
+    public static final double GROUND_SETPOINT = -70; // TODO
+    public static final double UPPER_ALGAE_SETPOINT = 3.927; // TODO
+    public static final double LOWER_ALGAE_SETPOINT = -39.144; // TODO
   }
 
   public static class IntakeConstants {
@@ -64,10 +64,9 @@ public final class Constants {
     public static final double INTAKE_PULSE_LENGTH = 0; // This value is in seconds
   }
 
- public static class ActuatorConstants {
-  public static final int length = 100; // Servo Length
- }
-
+  public static class ActuatorConstants {
+    public static final int length = 100; // Servo Length
+  }
 
   public static class ElevatorConstants {
     public static final int ELEVATOR_MOTOR_ID = 51;
@@ -81,9 +80,9 @@ public final class Constants {
     public static final double L3_SETPOINT = 102.275;
     public static final double L4_SETPOINT = 150;
     public static final double CORAL_STATION_SETPOINT = 30.272;
-    public static final double GROUND_SETPOINT = 20.115; //TODO
-    public static final double UPPER_ALGAE_SETPOINT = 10; //TODO
-    public static final double LOWER_ALGAE_SETPOINT = 10; //TODO
+    public static final double GROUND_SETPOINT = 20.115; // TODO
+    public static final double UPPER_ALGAE_SETPOINT = 90.941; // TODO
+    public static final double LOWER_ALGAE_SETPOINT = 29.57; // TODO
   }
 
   public static final class LimelightConstants {
